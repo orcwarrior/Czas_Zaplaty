ad60f987e05f0d3f9a406be9ec5faf56b2c64bde
Adanos <polon1@op.pl>
2013-12-07 18:51:56 +0100

    #33 - usuniecie NPC ze startupu.

--- Content/Story/Startup.d
+++ Content/Story/Startup.d
@@ -3869,7 +3869,7 @@ FUNC VOID STARTUP_DREAM01 ()
 	Wld_InsertNpc(RBL_1339_Novize, "PSI_PATH_1_1");
 	Wld_InsertNpc(RBL_1340_Novize, "PSI_PATH_1_1");
 
-	Wld_InsertNpc(RBL_1327_Novize, "PSI_PATH_1_1");
+	//Wld_InsertNpc(RBL_1327_Novize, "PSI_PATH_1_1");
 	Wld_InsertNpc(RBL_1328_Novize, "PSI_PATH_1_1");
 	Wld_InsertNpc(RBL_1329_Novize, "PSI_PATH_1_1");
 	Wld_InsertNpc(RBL_1332_BaalKagan, "PSI_PATH_1_1");
