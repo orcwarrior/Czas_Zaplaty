f39610f878bcbba4659083f4379cd49fee0b1096
Adanos <polon1@op.pl>
2014-12-10 09:11:57 +0100

    Poprawka #47

--- Content/Story/NPC/NEW_OUTWORLD/Grd_4079_Wegorz.d
+++ Content/Story/NPC/NEW_OUTWORLD/Grd_4079_Wegorz.d
@@ -2,13 +2,12 @@ instance Grd_2519_Wegorz (Npc_Default)
 {
 	//-------- primary data --------
 	
-	name 		=	"W?gorz";
+	name 		=	"Wêgorz";
 	npctype 	= 	npctype_main;
 	guild 		=	GIL_NONE;
 	level 		=	50;
 	voice 		=	11;
 	id 			=	2519;
-
 
 	//-------- abilities --------
 	attribute[ATR_STRENGTH] 	= 125;
@@ -25,9 +24,8 @@ instance Grd_2519_Wegorz (Npc_Default)
 	//			body mesh     ,bdytex,skin,head mesh     ,headtex,teethtex,ruestung	
 	Mdl_SetVisualBody (self,"hum_body_Naked0",0,1,"Hum_Head_FatBald",24,1,EBR_ARMOR_H2);
         
-    	B_Scale (self); 
+   B_Scale (self); 
 	Mdl_SetModelFatness(self,0);
-	
 
 	Mdl_SetModelScale(self,1.1,1.1,1.1);
 
@@ -36,7 +34,6 @@ instance Grd_2519_Wegorz (Npc_Default)
 	fight_tactic	=	FAI_HUMAN_MASTER;
 	
 	//-------- Talente --------  
-	
 	
 	Npc_SetTalentSkill (self,NPC_TALENT_2H,2);
 	Npc_SetTalentSkill (self,NPC_TALENT_1H,2);
@@ -47,11 +44,9 @@ instance Grd_2519_Wegorz (Npc_Default)
 	EquipItem	(self,GRD_MW_04);
 	CreateInvItems(self,ItFo_Potion_Health_03,4);
 
-
 	//-------------Daily Routine-------------
 	/*B_InitNPCAddins(self);*/		daily_routine = Rtn_Start_2519;
 };
-
 
 FUNC VOID Rtn_Start_2519 ()
 {
@@ -70,7 +65,6 @@ FUNC VOID Rtn_HIDE_2519 ()
 	TA_Guard	(00,00,06,00,"WP_INTRO_WI05");
 	TA_Guard	(06,00,24,00,"WP_INTRO_WI05");
 };
-
 
 FUNC VOID Rtn_OR3_2519 ()
 {
