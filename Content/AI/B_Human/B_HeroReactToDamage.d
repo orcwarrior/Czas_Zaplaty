0d2b5835f0e2ece2067e39f37a4e6f01794540f1
Adanos <polon1@op.pl>
2015-01-23 10:10:15 +0100

    Poprawka #177

--- Content/AI/B_Human/B_HeroReactToDamage.d
+++ Content/AI/B_Human/B_HeroReactToDamage.d
@@ -1,11 +1,11 @@
-//func void B_HeroReactToDamage ()
-//{
-//	////PrintDebugNpc		(PD_ZS_FRAME, "B_HeroReactToDamage" );	
-//	
-//	//PRINTGlobals 		(PD_ZS_CHECK);
-//	B_SpecialCombatDamageReaction(self,other);
-//};
-func void B_HeroReactToDamage()
+func void B_HeroReactToDamage ()
 {
-	//B_SpecialCombatDamageReaction(hero,oth);
+	////PrintDebugNpc		(PD_ZS_FRAME, "B_HeroReactToDamage" );	
+	
+	//PRINTGlobals 		(PD_ZS_CHECK);
+	B_SpecialCombatDamageReaction(self,other);
 };
+//func void B_HeroReactToDamage()
+//{
+	//B_SpecialCombatDamageReaction(hero,oth);
+//};
